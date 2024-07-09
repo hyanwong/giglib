1a9ff2c8a2a703b0063bd9bb5748b245924e2470
Yan Wong <yan.wong@bdi.ox.ac.uk>
2024-07-09 22:30:04 +0100

    Update flags to be uint32

--- GeneticInheritanceGraphLibrary/constants.py
+++ GeneticInheritanceGraphLibrary/constants.py
@@ -1,11 +1,12 @@
-from enum import IntFlag, auto
+from enum import IntFlag
 
+import numpy as np
 import tskit
 
 
 class Const:
-    NODE_IS_SAMPLE = tskit.NODE_IS_SAMPLE
-    NODE_IS_RE = 1 << 19  # mainly for testing
+    NODE_IS_SAMPLE = np.dtype("uint32").type(tskit.NODE_IS_SAMPLE)
+    NODE_IS_RE = np.dtype("uint32").type(1 << 19)  # mainly for testing
 
     #: The default NULL integer value, identical to :data:`tskit:tskit.NULL`
     NULL = tskit.NULL
@@ -24,50 +25,50 @@ class ValidFlags(IntFlag):
     # GIG DEFINITIONAL REQUIREMENTS
 
     #: If set, iedge data is guaranteed to be integers
-    IEDGES_INTEGERS = auto()
+    IEDGES_INTEGERS = np.dtype("uint32").type(1)  # TODO: replace with auto() for py3.12
 
     #: If set, intervals are guaranteed finite and have parent span == child span
-    IEDGES_INTERVALS = auto()
+    IEDGES_INTERVALS = np.dtype("uint32").type(1 << 1)
 
     #: IF set, each genomic position in a child's chromosome is guaranteed to have
     #: only one interval above
-    IEDGES_FOR_CHILD_NONOVERLAPPING = auto()
+    IEDGES_FOR_CHILD_NONOVERLAPPING = np.dtype("uint32").type(1 << 2)
 
     #: If set, all parents are older than their children, guaranteeing the GIG is acyclic
     #: (note this requires knowledge of the times in the nodes table). This flag also
     #: guarantees that nodes with those parent and child IDs exist in the nodes table.
-    IEDGES_PARENT_OLDER_THAN_CHILD = auto()
+    IEDGES_PARENT_OLDER_THAN_CHILD = np.dtype("uint32").type(1 << 3)
 
     #: If set, iedges with the same edge_ID are guaranteed to have the same combination
     #: of parentID / childID. Note the inverse may not be true: iedges with the same
     #: parentID / childID can have different edge_IDs.
-    IEDGES_SAME_PARENT_CHILD_FOR_EDGE = auto()
+    IEDGES_SAME_PARENT_CHILD_FOR_EDGE = np.dtype("uint32").type(1 << 4)
 
     # IEDGE EFFICIENCY REQUIREMENTS (e.g. to define a canonical iedge order)
 
     #: Set if ``child_left`` < ``child_right`` (inversions can have
     #: ``parent_left`` > ``parent_right``)
-    IEDGES_CHILD_INTERVAL_POSITIVE = auto()
+    IEDGES_CHILD_INTERVAL_POSITIVE = np.dtype("uint32").type(1 << 5)
 
     #: Set if all iedges with the same child ID are adjacent in the table
-    IEDGES_FOR_CHILD_ADJACENT = auto()
+    IEDGES_FOR_CHILD_ADJACENT = np.dtype("uint32").type(1 << 6)
 
     #: Set if within a set of iedges for the same child, iedges are ordered first
     #: by chromosome number
-    IEDGES_FOR_CHILD_PRIMARY_ORDER_CHR_ASC = auto()
+    IEDGES_FOR_CHILD_PRIMARY_ORDER_CHR_ASC = np.dtype("uint32").type(1 << 7)
 
     #: Set if, within a set of iedges for the same child & chromosome, iedges are ordered
     #: by left position
-    IEDGES_FOR_CHILD_SECONDARY_ORDER_LEFT_ASC = auto()
+    IEDGES_FOR_CHILD_SECONDARY_ORDER_LEFT_ASC = np.dtype("uint32").type(1 << 8)
 
     #: Set if the adjacent rows for a child are ordered in descending order of child
     #: node time. Note that this requires knowledge of the times in the nodes table.
-    IEDGES_PRIMARY_ORDER_CHILD_TIME_DESC = auto()
+    IEDGES_PRIMARY_ORDER_CHILD_TIME_DESC = np.dtype("uint32").type(1 << 9)
 
     #: Set if iedges are ordered such that where iedge children are tied at the same
     #: time, iedges with the lowest child ID come first. Note that although this is often
     #: not algorithmically necessary, it helps to creates a canonical iedge ordering.
-    IEDGES_SECONDARY_ORDER_CHILD_ID_ASC = auto()
+    IEDGES_SECONDARY_ORDER_CHILD_ID_ASC = np.dtype("uint32").type(1 << 10)
 
     IEDGES_ALL = (
         IEDGES_INTEGERS
